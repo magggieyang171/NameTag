cdd1bfc48ff91b0e9952c4e5106c3db3f0ee8760
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2023-03-12 10:24:32 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    basic.showString("Maggie")
+    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+    basic.clearScreen()
+})
